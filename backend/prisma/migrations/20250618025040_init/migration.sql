b4747881c45077aa46a958ddce43136fa599c002
えがしら0919 <@example.com>
2025-06-18 14:51:25 +0900

    Add: backend構築

---
+++ backend/prisma/migrations/20250618025040_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `account_number` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Project` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `goal_amount` INTEGER NOT NULL,
+    `deadline` DATETIME(3) NOT NULL,
+    `current_amount` INTEGER NOT NULL,
+    `project_account_number` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
