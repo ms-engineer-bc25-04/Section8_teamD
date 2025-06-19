1d1ec2f86dd75c6316edf4ebd3743017012f8eef
TakaeShiobara <takaeshiobara@gmail.com>
2025-06-19 11:23:12 +0900

    [Add]firebase認証の導入

---
+++ backend/middleware/authenticateFirebaseToken.ts
@@ -0,0 +1,15 @@
+import { admin } from "../src/lib/firebase-admin";
+
+export async function authenticateFirebaseToken(req, res, next) {
+  const idToken = req.headers.authorization?.split("Bearer ")[1];
+  if (!idToken) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken;
+    next();
+  } catch (e) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+}
