44a8eade60a7155e41b3d3837e8610f96ecc4871
half-kiwi1219 <afuafuafro20110629@gmail.com>
2025-06-19 11:55:46 +0900

    projectとuserテーブルのmockデータフォルダ追加

---
+++ frontend/src/app/mocks/user.ts
@@ -0,0 +1,32 @@
+export const mockUsers = [
+  {
+    id: "u1",
+    name: "シオバラタカエ",
+    email: "takae@example.com",
+    password: "pass1234", // ★仮実装用。将来は使わない
+    accountNumber: "0010658",
+    beneficiaryBranchCode: 302,
+    access_token: "YTAzZjYxMTZkN...",
+    balance: 100000,
+  },
+  {
+    id: "u2",
+    name: "エガシラノリコ",
+    email: "noriko@example.com",
+    password: "nori5678",
+    accountNumber: "0012010",
+    beneficiaryBranchCode: 102,
+    access_token: "NTNmYVWMIM2UxO...",
+    balance: 100000,
+  },
+  {
+    id: "u3",
+    name: "シマダアユミ",
+    email: "ayumi@example.com",
+    password: "ayu9876",
+    accountNumber: "0010689",
+    beneficiaryBranchCode: 302,
+    access_token: "OTU5ZGlxYTcxYj...",
+    balance: 90000,
+  },
+];
